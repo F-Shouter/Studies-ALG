8890035c8951d76c09a4756723e4d49df9c64f41
F-Shouter <yukari.yamasaki1809@gmail.com>
2025-02-27 15:53:03 -0300

    a

--- Aula06/Lista04_Parte3/script.js
+++ Aula06/Lista04_Parte3/script.js
@@ -7,3 +7,32 @@ function comprar(){
     }else {total = qtd * 3;}
     div_mensagem.innerHTML = `O valor da sua compra foi de R$${total} reais.`;
 }
+/* Exercício 17 */
+function analisar(){
+    var freq = Number(input_freq.value);
+    if(freq>=50){
+        div_mensagem.innerHTML = `<img src="image/images.jpg" alt="">`;
+    }else {
+        div_mensagem.innerHTML = `<img src="image/download.jpg" alt="">`;
+    }
+}
+/* Exercício 18 */
+function verificar(){
+    var n1 = Number(input_n1.value);
+    var n2 = Number(input_n2.value);
+    var media = (n1+n2)/2;
+    if(media >=6){
+        div_mensagem.innerHTML = `Parabéns, aprovado!`;
+    }else {
+        div_mensagem.innerHTML = `Tente outra vez...`;
+    }
+}
+/* Exercício 19 */
+function vmedir(){
+    var temp = Number(input_temp.value);
+    if(temp < 10){
+        div_mensagem.innerHTML = `Apropriada p/ conservar alimento`;
+    }else {
+        div_mensagem.innerHTML = `Tente outra vez...`;
+    }
+}
