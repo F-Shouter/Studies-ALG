13f35629a5af680f6ca6a2552a6386425c8c4283
F-Shouter <yukari.yamasaki1809@gmail.com>
2025-06-11 13:51:10 -0300

    documents add

---
+++ Aula20/retorno_parametro.js
@@ -0,0 +1,18 @@
+console.log('oi');
+console.log('oi', 'tudo', 'bem?');
+
+function cumprimentar(mensagem){
+    console.log(`Mensagem: ${mensagem}`);
+}
+cumprimentar('Oloko');
+
+function multiplicar(n1, n2){
+    var multiplic = n1 * n2;
+    console.log(`Multiplicação de: ${n1} * ${n2}`);
+    return multiplic;
+}
+
+// let resultado = multiplicar(5, 9);
+// console.log('O resultado é:', resultado);
+
+console.log(multiplicar(5,8));
